64dc9cf792fa955329e5690488b86e31caf30b3c
Caio <vitor.da.alvorada@gmail.com>
2021-04-15 14:02:51 -0300

    Eitcha

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
-FROM php:8.0.0rc1-fpm
+FROM php:7.4.16-fpm-buster
+RUN apt-get update -y && apt-get install -y libmcrypt-dev openssl
+RUN pecl install mcrypt-1.0.3
+RUN command docker-php-ext-enable mcrypt
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+WORKDIR /app
+COPY . /app
+RUN composer install
 
-# Install system dependencies
-RUN apt-get update && apt-get install -y git
-
-# Install PHP extensions
-RUN docker-php-ext-install pdo_mysql
-
-# Get latest Composer
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
-
-# Set working directory
-WORKDIR /var/www
+CMD php artisan serve --host=0.0.0.0 --port=8000
+EXPOSE 8000
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,17 @@
 <?php
-  
+
 namespace App\Models;
-  
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
-  
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-  
+    use HasFactory, Notifiable, HasApiTokens;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-  
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -32,7 +32,7 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-  
+
     /**
      * The attributes that should be cast to native types.
      *
